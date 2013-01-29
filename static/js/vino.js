7cd9afa63ac4574234d87b513ef8d942497aa6b3
Birk Nilson <birk@tictail.com>
2013-01-29 21:07:10 +0100

    Minor frontend change

--- static/js/vino.js
+++ static/js/vino.js
@@ -1,13 +1,22 @@
+// Array Remove - By John Resig (MIT Licensed)
+Array.prototype.remove = function(from, to) {
+    var rest = this.slice((to || from) + 1 || this.length);
+    this.length = from < 0 ? this.length + from : from;
+    return this.push.apply(this, rest);
+};
+
 var Vino = (function($) {
     var video = 'http://vines.s3.amazonaws.com/videos_500k/A50E183B-9655-4677-AD76-50BFADDE847C-999-00000067BE55B8F8_1.0.3.mp4?versionId=5yZiEhDHzx60D.tuFt3NJPa1Lh3MAdgk';
 
     var cls = function(options) {
-        this.data = {};
+        this.queue = [];
+        this.onScreen = [];
+
         this.options = options;
         this.videoContainer = $('#videos');
         this.documentNode = $(document);
 
-        this.videosPerRow = this.options.videosPerRow || 3;
+        this.videosPerRow = this.options.videosPerRow || 5;
 
         this.synchronizeCanvasSize();
         this.initEventListeners();
@@ -74,7 +83,22 @@ var Vino = (function($) {
         },
 
         store: function(data) {
-            this.data = data;
+            var records = data.records;
+            if (!records) {
+                return false;
+            }
+
+            var normalized, count, queue;
+
+            queue = this.queue;
+            count = records.length;
+            for (var i = 0; i < count; i++) {
+                current = records[i];
+                queue.push({
+                    id: current.postId,
+                    video: current.videoUrl
+                });
+            }
         },
 
         calculateVideoWidth: function() {
@@ -85,27 +109,36 @@ var Vino = (function($) {
             var container = this.videoContainer;
             container.empty();
 
-            var records = this.data.records;
-            if (!records) {
-                console.log('No records');
+            this.draw();
+        },
+
+        draw: function() {
+            var queue = this.queue;
+            if (!queue.length) {
                 return;
             }
 
-            console.log('Redrawing');
-            var width = this.calculateVideoWidth();
+            var container, width, count, current, onScreen;
 
-            var count = records.length;
+            onScreen = this.onScreen;
+            container = this.videoContainer;
+            width = this.calculateVideoWidth();
+            count = queue.length;
+
             for (var i = 0; i < count; i++) {
-                html = this.generateVideoHtml(records[i], width);
+                current = queue[i];
+                html = this.generateVideoHtml(current, width);
                 container.append(html);
+
+                queue.remove(i);
+                onScreen.push(current);
             }
         },
 
         generateVideoHtml: function(record, width) {
-            console.log(width);
             var markup = '<li>'
                         + '<video autoplay loop muted width="' + width + '">'
-                        + '<source src="' + record.videoUrl + '">'
+                        + '<source src="' + record.video + '">'
                         + '</video>'
                         + '</li>';
 
